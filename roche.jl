20299d2bb73e45cb2e4e60d1e2f49cd71356fb8c
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-06-11 17:23:47 -0400

    Update roche.jl

--- roche.jl
+++ roche.jl
@@ -1,3 +1,5 @@
+using Gadfly
+
 function roche_distance(R, M, m)
 	d = R * (2*M/m)^(1/3)
 	return d
@@ -8,5 +10,22 @@ earth_mass = 5.97e24				# Earth mass (kg)
 
 moon_mass = 7.34e22					# Moon mass (Kg)
 
-print("The Moon's Roche limit is ")
-println(round(roche_distance(earth_radius, earth_mass, moon_mass), digits=3), " Km")
+#print("The Moon's Roche limit is ")
+#println(round(roche_distance(earth_radius, earth_mass, moon_mass), digits=3), " Km")
+
+# for i in 1:100
+# 	println(round(roche_distance(earth_radius, earth_mass, i*1e20), digits=3))
+# end
+
+roche = []
+
+# for i in 1:100
+# 	push!(roche, roche_distance(earth_radius, earth_mass, i*1e21))
+# end
+
+# plot(y=roche)
+
+plot([sin,cos], 0, 2pi)
+
+println("Waiting for key press ...")
+readline()
