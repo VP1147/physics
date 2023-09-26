af3e4f8e3e0cb0d454f076b6fddb02d62edb3f29
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-09-26 13:12:24 -0400

    Update eletromag.c

--- eletromag.c
+++ eletromag.c
@@ -1,3 +1,19 @@
+/*
+Eletromagnetic oscilation simulator 
+Copyright (C) 2023 Vinícius Pavão
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
+
+// gcc -o eletromag eletromag.c -I/usr/include/SDL2 -D_REENTRANT -lSDL2 -lSDL2_ttf -lm
+
 #include <SDL.h>
 #include <SDL_ttf.h>
 #include <stdio.h>
